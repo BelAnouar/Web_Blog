5001e04dca9e7ea47a9edf19a2993a67d70cbde1
anouar <anouar.ab95@gmail.com>
2024-10-07 17:17:06 +0100

    WB-14 <realiser larchitecture >

--- src/main/java/com/webblog/controllers/Servletarticle.java
+++ src/main/java/com/webblog/controllers/Servletarticle.java
@@ -9,33 +9,25 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Servletarticle extends HttpServlet {
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public Servletarticle() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
+
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
